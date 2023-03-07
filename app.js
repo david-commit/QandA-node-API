1dd04cb3f740d445b79a33f056595dd0bfa2fa00
david-commit <davidondiege@gmail.com>
2023-03-07 12:26:05 +0300

    Added an authenticated route file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const globalRoutes = require('./routes/routes')
+const authRoutes = require('./routes/auth')
 
 // Configuration
 dotenv.config()
@@ -10,6 +11,7 @@ dotenv.config()
 // Middleware
 app.use(bodyParser.json())
 app.use('/', globalRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
- res.json({
-   name: 'Muhammad Ishaq',
-   gender: 'Male',
-   age: 23,
-   address: {
-     street: '87',
-     city: 'Gultari Matyal Skardu',
-     state: 'Gilgit Baltistan',
-     postalCode: '16350',
-   },
-   phoneNumber: [{ type: 'personal', number: '116263747' }],
- });
+router.get('/questions', (req, res) => {
+   res.json({
+     id: 1,
+     user_id: 1,
+     question:
+       'What is the average experience needed to be a Principal Software Engineer?',
+     answers: [
+       {
+         id: 1,
+         user_id: 2,
+         answer: "I'd say 5 years on the job",
+       },
+       {
+         id: 2,
+         user_id: 3,
+         answer: "I'd say 5 years on the job",
+       },
+       {
+         id: 3,
+         user_id: 2,
+         answer: "Correction, Indeed suggests 7 years",
+       },
+     ],
+   });
 })
 
 module.exports = router
