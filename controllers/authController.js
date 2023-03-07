8f670c928894863292dcb814215c243fde0e1997
david-commit <davidondiege@gmail.com>
2023-03-07 12:44:45 +0300

    Added an authentication controller draft

---
+++ controllers/authController.js
@@ -0,0 +1,7 @@
+const postQuestion = (req, res) => {
+ res.json({
+  message: "You're posting a question"
+ })
+};
+
+module.exports = { postQuestion }
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { postQuestion } = require('../controllers/authController')
 
-router.get('/', (req, res) => {
-  res.json({
-    id: 1,
-    user_id: 1,
-    question:
-      'What is the average experience needed to be a Principal Software Engineer?',
-    answers: [
-      {
-        id: 1,
-        user_id: 2,
-        answer: "I'd say 5 years on the job",
-      },
-      {
-        id: 2,
-        user_id: 3,
-        answer: "I'd say 5 years on the job",
-      },
-      {
-        id: 3,
-        user_id: 2,
-        answer: 'Correction, Indeed suggests 7 years',
-      },
-    ],
-  });
-});
+router.post('/', postQuestion);
 
 module.exports = router;
