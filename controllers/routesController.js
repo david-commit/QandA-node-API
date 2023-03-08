389716a91cae26d5102f7934e0afaaa53675e375
david-commit <davidondiege@gmail.com>
2023-03-09 00:17:46 +0300

    Successfuly added the answer POST request, with auth

--- controllers/routesController.js
+++ controllers/routesController.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
+const { check, validationResult } = require('express-validator');
 
 // ====> Post question controller & Validation
 const postQuestion = async (req, res) => {
@@ -31,19 +32,19 @@ const postQuestion = async (req, res) => {
 };
 
 const getQuestions = async (req, res) => {
- const questions = await prisma.question.findMany({
-  select: {
-    id: true,
-    question: true,
-    user_id: true,
-    answers: true
-  }
- })
- res.json(questions)
+  const questions = await prisma.question.findMany({
+    select: {
+      id: true,
+      question: true,
+      user_id: true,
+      answers: true,
+    },
+  });
+  res.json(questions);
 };
 
 const getQuestion = async (req, res) => {
-  const { q_id } = req.params
+  const { q_id } = req.params;
 
   // Find single question
   const question = await prisma.question.findUnique({
@@ -55,65 +56,125 @@ const getQuestion = async (req, res) => {
       question: true,
       created_at: true,
       user_id: true,
-      answers: true
+      answers: true,
     },
   });
 
   if (!question) {
     return res.status(400).json({
-      msg: "Question does not exist"
-    })
+      msg: 'Question does not exist',
+    });
   }
-  
+
   res.json(question);
 };
 
 // ====> Delete question controller & Validation
 const deleteQuestion = async (req, res) => {
-  const { q_id } = req.params
+  const { q_id } = req.params;
 
   // Check if user is authenticated
- 
 
   // Find question by ID
   const question = await prisma.question.findUnique({
     where: {
-      id: parseInt(q_id)
-    }
-  })
-  
+      id: parseInt(q_id),
+    },
+  });
+
   // Check if question exists
   if (!question) {
     return res.status(400).json({
-      msg: "Question does not exist"
-    })
+      msg: 'Question does not exist',
+    });
   }
 
   // Proceed to delete
   await prisma.question.delete({
     where: {
-      id: parseInt(q_id)
-    }
-  })
+      id: parseInt(q_id),
+    },
+  });
 
   res.json({
-    msg: "Question deleted successfully"
-  })
+    msg: 'Question deleted successfully',
+  });
+};
+
+// ====> Post answer controller & Validation
+const postAnswer = async (req, res) => {
+  // Grab question ID from params
+  const { q_id } = req.params;
+  const { answer, question_id, created_at } = req.body;
+  // Validate the input request
+  const errors = validationResult(req);
+
+  //  Check if the question exists
+  let question = await prisma.question.findUnique({
+    where: {
+      id: parseInt(q_id),
+    },
+  });
+
+  if (!question) {
+    return res.status(400).json({
+      msg: 'Question does not exist',
+    });
+  }
+
+  const newAnswer = await prisma.answer.create({
+    data: { answer, question_id: q_id, created_at },
+  });
+
+  res.json(newAnswer);
+};
+const postAnswerValidation = [
+  check('answer', 'Name must be more than 3 characters')
+    .trim()
+    .isLength({ min: 3 }),
+];
+
+// ====> Update answer controller & Validation
+const updateAnswer = (req, res) => {
+  const errors = validationResult(req);
+
+  res.json({
+    message: "You're editing an answer to a question",
+  });
+};
+const updateAnswerValidation = [
+  check('full_name', 'Name must be more than 3 characters')
+    .trim()
+    .isLength({ min: 3 }),
+  check('email', 'Invalid email').isEmail(),
+  check('password', 'Password must contain more than 6 characters').isLength({
+    min: 6,
+  }),
+  check('role', 'Please stste your role in/to the community').isLength({
+    min: 2,
+  }),
+];
+
+// ====> Delete answer controller & Validation
+const deleteAnswer = (req, res) => {
+  res.json({
+    message: "You're deleting an answer to a question",
+  });
 };
 
 const getTest = async (req, res) => {
   const users = await prisma.user.findMany({
     select: {
-      id: true, 
+      id: true,
       full_name: true,
-      email: true, 
+      email: true,
       phone: true,
       role: true,
-      questions: true
-    }
-  })
-  res.json(users)
-}
+      questions: true,
+    },
+  });
+  res.json(users);
+};
 
 module.exports = {
   getQuestions,
@@ -121,4 +182,9 @@ module.exports = {
   getTest,
   postQuestion,
   deleteQuestion,
+  postAnswer,
+  updateAnswer,
+  deleteAnswer,
+  updateAnswerValidation,
+  postAnswerValidation,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,36 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const {
   getQuestions,
   getQuestion,
   getTest,
   postQuestion,
   deleteQuestion,
+  postAnswer,
+  updateAnswer,
+  deleteAnswer,
+  updateAnswerValidation,
+  postAnswerValidation,
 } = require('../controllers/routesController');
+const checkAuth = require('../middleware/checkAuth');
 
-router.get('/questions', getQuestions)
-router.get('/questions/:q_id', getQuestion)
-router.post('/questions', postQuestion);
-router.delete('/questions/:q_id', deleteQuestion);
-router.get('/test', getTest)
+router.get('/questions', getQuestions);
+router.get('/questions/:q_id', getQuestion);
+router.post('/questions', checkAuth, postQuestion);
+router.delete('/questions/:q_id', checkAuth, deleteQuestion);
+router.post('/questions/:q_id/answers', checkAuth, postAnswer);
+router.put(
+  '/questions/:q_id/answers/:a_id',
+  checkAuth,
+  updateAnswerValidation,
+  updateAnswer
+);
+router.delete(
+  '/questions/:id/answers/:id',
+  checkAuth,
+  postAnswerValidation,
+  deleteAnswer
+);
+router.get('/test', getTest);
 
-module.exports = router
+module.exports = router;
