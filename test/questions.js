96e86247fa753b430abe2fe0478955c850218a15
david-commit <davidondiege@gmail.com>
2023-03-14 12:24:56 +0300

    Added a throw 404 error test on question

--- test/questions.js
+++ test/questions.js
@@ -9,7 +9,7 @@ chai.should();
 // Middleware
 chai.use(chaiHttp);
 
-describe('Question Responses', () => {
+describe('Question Resources', () => {
   // GET all Route
   describe('GET all questions', () => {
     it('Should get all questions', (done) => {
@@ -25,7 +25,7 @@ describe('Question Responses', () => {
   });
 
   // GET single Route
-  describe('GET all questions', () => {
+  describe('GET specific question', () => {
     it('Should get single question by ID', (done) => {
       const qId = 1 || 2 || 3;
       chai
@@ -36,9 +36,22 @@ describe('Question Responses', () => {
           response.body.should.be.a('object');
           response.body.should.have.property('id');
           response.body.should.have.property('user_id');
-          response.body.should.have.property('answers').that.is.a('array')
+          response.body.should.have.property('answers').that.is.a('array');
           done();
         });
     });
+  });
+
+  it("Should respond question not found", (done) => {
+    const qId = 1000;
+    chai
+      .request(server)
+      .get(`/questions/${qId}`)
+      .end((err, response) => {
+        response.should.have.status(404);
+        response.body.should.be.a('object');
+        response.body.should.have.property('msg');
+        done();
+      });
   });
 });
