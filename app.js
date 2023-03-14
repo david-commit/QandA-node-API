f86ca50e88cf5da878cec5335173231b5244acc2
david-commit <davidondiege@gmail.com>
2023-03-14 11:26:05 +0300

    [fix] TypeError: app.address is not a function

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const globalRoutes = require('./routes/routes')
-const authRoutes = require('./routes/auth')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const globalRoutes = require('./routes/routes');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+const morgan = require('morgan');
 
 // Configuration
-dotenv.config()
+dotenv.config();
 
 // Middleware
 // app.use(express.json())
-app.use(morgan('dev'))
-app.use(cors({origin: '*'}))
-app.use(bodyParser.json())
-app.use('/', globalRoutes)
-app.use('/auth', authRoutes)
+app.use(morgan('dev'));
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.json());
+app.use('/', globalRoutes);
+app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
- console.log(`Node server running on http://localhost:${PORT}`)
-})
+if (!module.parent) {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Node server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
